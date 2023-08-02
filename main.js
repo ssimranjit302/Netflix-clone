4cf1363c198dfc1b9f24e2caad50eddb11f550bf
Simranjit Singh <simran@Simranjits-MacBook-Air.local>
2023-08-02 12:50:06 +0530

    images added

--- main.js
+++ main.js
@@ -195,6 +195,82 @@
         });
 
 
+        jQuery("#trending-slider").slick({
+            slidesToShow : 1,
+            slidesToScroll : 1,
+            arrows : false,
+            fade : true,
+            draggable : false,
+            asNavFor : "#trending-slider-nav",
+        });
+
+        jQuery("#trending-slider-nav").slick({
+            slidesToShow : 5,
+            slidesToScroll : 1,
+            asNavFor : "#trending-slider",
+            dots : false ,
+            arrows : true ,
+            nextArrow: '<a href="#" class="slick-arrow slick-next"><i class="fa fa-chevron-right"></i></a>',
+            prevArrow: '<a href="#" class="slick-arrow slick-prev"><i class="fa fa-chevron-left"></i></a>',
+            infinite : true,
+            centerMode : true,
+            centerPadding : 0,
+            focusOnSelect : true,
+            responsive : [
+                {
+                    breakpoint : 1024,
+                    settings : {
+                        slidesToShow : 2,
+                        slidesToScroll : 1,
+                    }
+                },
+                {
+                    breakpoint : 600,
+                    settings : {
+                        slidesToShow : 1,
+                        slidesToScroll : 1,
+                    }
+                }
+            ]
+        });
+
+        jQuery('.episodes-slider1').owlCarousel({
+            loop : true,
+            margin : 20,
+            nav: true,
+            navText : ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i> "],
+            dots : false,
+            responsive : {
+                0:{
+                    items : 1
+                },
+                600: {
+                    items : 1
+                },
+                1000 : {
+                    items : 4
+                }
+            }
+        });
+
+
+        jQuery('.trending-content').each(function(){
+            var highestBox = 0;
+            jQuery('.tab-pane', this).each(function(){
+                if(jQuery(this).height() > highestBox){
+                    highestBox = jQuery(this).height();
+                }
+            });
+            jQuery('.tab-pane', this).height(highestBox);
+        });
+
+        if(jQuery('select').hasClass('season-select')){
+            jQuery('select').select2({
+                theme : 'bootstrap4',
+                allowClear : false,
+                width : 'resolve'
+            });
+        }
 
 
     });
